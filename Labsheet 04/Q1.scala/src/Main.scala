ebad85e5fddecae92de5666c2e35e5141606923f
Anjula-Himashi <140165333+Anjula-Himashi@users.noreply.github.com>
2024-07-11 18:45:30 +0530

    Update Main.scala

--- Labsheet 04/Q1.scala/src/Main.scala
+++ Labsheet 04/Q1.scala/src/Main.scala
@@ -1,51 +1,52 @@
 object InventoryManagement {
 
-  var itemNames = Array("apple", "banana", "orange")
-  var itemQuantities = Array(10, 5, 8)
+  var itemNames: Array[String] = Array("Apples", "Bananas", "Oranges")
+  var itemQuantities: Array[Int] = Array(10, 20, 15)
 
-  // Function to display the inventory
   def displayInventory(): Unit = {
     println("Inventory:")
-    for ((name, quantity) <- itemNames zip itemQuantities) {
-      println(s"$name: $quantity")
+    for (i <- itemNames.indices) {
+      println(s"${itemNames(i)}: ${itemQuantities(i)}")
     }
-    println()
   }
-
 
   def restockItem(itemName: String, quantity: Int): Unit = {
     val index = itemNames.indexOf(itemName)
-    if (index >= 0) {
+    if (index != -1) {
       itemQuantities(index) += quantity
-      println(s"Restocked $quantity of $itemName. New quantity: ${itemQuantities(index)}")
+      println(s"$quantity units of $itemName added. New quantity: ${itemQuantities(index)}")
     } else {
-      println(s"Item '$itemName' does not exist in the inventory.")
+      println(s"$itemName does not exist in the inventory.")
     }
-    println()
   }
-
 
   def sellItem(itemName: String, quantity: Int): Unit = {
     val index = itemNames.indexOf(itemName)
-    if (index >= 0) {
+    if (index != -1) {
       if (itemQuantities(index) >= quantity) {
         itemQuantities(index) -= quantity
-        println(s"Sold $quantity of $itemName. Remaining quantity: ${itemQuantities(index)}")
+        println(s"$quantity units of $itemName sold. Remaining quantity: ${itemQuantities(index)}")
       } else {
-        println(s"Not enough quantity of $itemName to sell. Available quantity: ${itemQuantities(index)}")
+        println(s"Not enough $itemName in stock to sell $quantity units.")
       }
     } else {
-      println(s"Item '$itemName' does not exist in the inventory.")
+      println(s"$itemName does not exist in the inventory.")
     }
-    println()
   }
 
-
   def main(args: Array[String]): Unit = {
+    
     displayInventory()
-    restockItem("banana", 10)
-    sellItem("orange", 5)
-    sellItem("apple", 15)
+
+    restockItem("Apples", 5)
+
+    restockItem("Grapes", 10)
+
+    sellItem("Bananas", 10)
+
+    sellItem("Oranges", 20)
+
     displayInventory()
   }
 }
+
